16b33a209d4ae5af204c4c32290d25a3b83ea48d
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-04-17 13:45:43 +0100

    adding db name

--- src/models/EnvironmentContext.js
+++ src/models/EnvironmentContext.js
@@ -3,6 +3,7 @@ class EnvironmentContext {
     constructor(env) {
         this.referenceDataUrl = env.REFERENCE_DATA_URL;
         this.workflowUrl = env.WORKFLOW_URL;
+        this.prestDbName = env.PRES_DB_NAME;
     }
 }
 
