be6d406557c2dac1f4c7acdd4d2ef86d3955c8ff
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-04-17 09:40:06 +0100

    added some extra logging

--- src/services/FormEngineService.js
+++ src/services/FormEngineService.js
@@ -7,7 +7,8 @@ const loadForm = async (formName) => {
     try {
         const response = await axios.get(`${process.env.FORM_URL}/form?name=${formName}`);
         return response.data ? response.data[0] : null;
-    } catch (e) {
+    } catch (e)  {
+        logger.error(`An exception occurred while trying to get form '%s' ... '%s'`, formName, e);
         throw e;
     }
 };
