4aad6c0f786bd35a68f186b4c38522b9804c52b7
Leigh Anderson <landerson@equalexperts.com>
2019-04-17 09:44:38 +0100

    Add a request correlation id to logging

--- src/config/winston.js
+++ src/config/winston.js
@@ -1,11 +1,18 @@
 import {createLogger, format, transports} from 'winston';
+import Tracing from '../utilities/tracing.js';
+
 const { combine, timestamp, json, splat, printf} = format;
 
+const addCorrelationId = format((info) => {
+    info.correlationId = Tracing.correlationId();
+    return info;
+});
 
 const logger = createLogger({
     format: combine(
         timestamp(),
         splat(),
+        addCorrelationId(),
         json()
     ),
     transports: [
