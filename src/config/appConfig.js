de3ee3f4bbfdf5a65b05e0a9f16db03e22351d00
shepda <david@x-designs.co.uk>
2019-05-16 15:35:20 +0100

    Update incorrectly named env vars and remove some that are not required

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,29 +1,21 @@
 const {
     PROTOCOL,
     INT_DOMAIN,
-    FORMIO_SERVER_NAME,
-    OPERATIONAL_POSTGREST_NAME,
-    REPORTING_SERVER_NAME,
-    TRANSLATION_SERVER_NAME,
-    WORKFLOW_SERVER_NAME,
+    PRIVATE_FORM_NAME,
+    PRIVATE_POSTGREST_NAME,
+    PRIVATE_WORKFLOW_ENGINE_NAME,
 } = process.env;
 
 const appConfig = {
     services: {
         operationalData: {
-            url: `${PROTOCOL}${OPERATIONAL_POSTGREST_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_POSTGREST_NAME}.${INT_DOMAIN}`,
         },
         workflow: {
-            url: `${PROTOCOL}${WORKFLOW_SERVER_NAME}.${INT_DOMAIN}`,
-        },
-        translation: {
-            url: `${PROTOCOL}${TRANSLATION_SERVER_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_WORKFLOW_ENGINE_NAME}.${INT_DOMAIN}`,
         },
         form: {
-            url: `${PROTOCOL}${FORMIO_SERVER_NAME}.${INT_DOMAIN}`,
-        },
-        report: {
-            url: `${PROTOCOL}${REPORTING_SERVER_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_FORM_NAME}.${INT_DOMAIN}`,
         },
         referenceData: {
             url: ''
