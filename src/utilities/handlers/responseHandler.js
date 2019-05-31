474245e0ba9b88d55f82113c00513cea93ac7709
David Shepherd <david@x-designs.co.uk>
2019-05-31 16:48:28 +0100

    Set a default error status code

--- src/utilities/handlers/responseHandler.js
+++ src/utilities/handlers/responseHandler.js
@@ -1,6 +1,8 @@
+const errorStatusCode = (code) => code || 500;
+
 const res = (err, {formName, form}, res) => {
     if (err) {
-        return res.status(err.code).json(err.message);
+        return res.status(errorStatusCode(err.code)).json(err.message);
     }
     if (!form) {
         return res.status(404).json({'message': `form ${formName} does not exist`});
@@ -11,7 +13,7 @@ const res = (err, {formName, form}, res) => {
 
 const healthRes = (err, data, res) => {
     if (err) {
-        return res.status(err.code).json(err.message);
+        return res.status(errorStatusCode(err.code)).json(err.message);
     }
     return res.status(200).json(data);
 };
