0c73879cd620a7da83d5cea83618fed6577b13b7
shepda <david@x-designs.co.uk>
2019-06-03 09:26:40 +0100

    Add more logging

--- src/routes/formDataResolveRouter.js
+++ src/routes/formDataResolveRouter.js
@@ -11,9 +11,15 @@ const formDataResolveRouter = (keycloak, formTranslatorController) => {
             [keycloak.protect(), (req, res) => {
                 const {formName} = req.params;
                 formTranslatorController.getForm(req).then((form) => {
+                    logger.info('In formTranslatorController.getForm');
+                    logger.info(form);
                     logger.info(`Sending form ${form.name} to responseHandler`);
                     responseHandler.res(null, {formName, form}, res);
                 }).catch((err) => {
+                    logger.info('Error in formTranslatorController.getForm');
+                    logger.info(err.code);
+                    logger.info(err.message);
+                    logger.info('-----------');
                     responseHandler.res({
                         code: err.code,
                         message: err.message
@@ -24,8 +30,14 @@ const formDataResolveRouter = (keycloak, formTranslatorController) => {
     router
         .post('/form', [keycloak.protect(), (req, res) => {
             formTranslatorController.getFormWithContext(req).then((form) => {
+                logger.info('In formTranslatorController.getFormWithContext');
+                logger.info(form);
                 responseHandler.res(null, {form}, res);
             }).catch((err) => {
+                logger.info('Error in formTranslatorController.getFormWithContext');
+                logger.info(err.code);
+                logger.info(err.toString());
+                logger.info('-----------');
                 responseHandler.res({
                     code: err.code,
                     message: err.toString()
