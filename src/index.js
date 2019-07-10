6374e83a90bc8694711776d949bc454bc29d2d96
Mark Olliver <mark@digitalpatterns.io>
2019-07-10 10:07:05 +0100

    remove ca bundle

--- src/index.js
+++ src/index.js
@@ -22,18 +22,6 @@ import logger from './config/winston';
 import Tracing from "./utilities/tracing";
 import cors from 'cors';
 
-if (process.env.NODE_ENV === 'production') {
-    logger.info('Setting ca bundle');
-    const trustedCa = [
-        '/etc/ssl/certs/ca-bundle.crt'
-    ];
-
-    https.globalAgent.options.ca = [];
-    for (const ca of trustedCa) {
-        https.globalAgent.options.ca.push(fs.readFileSync(ca));
-    }
-    logger.info('ca bundle set...');
-}
 const kcConfig = {
     clientId: process.env.AUTH_CLIENT_ID,
     serverUrl: process.env.AUTH_URL,
