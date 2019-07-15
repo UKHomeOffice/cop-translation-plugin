88526d4c5d0fba353128897def8fbe2884e03e07
Mark Olliver <mark@digitalpatterns.io>
2019-07-15 14:48:12 +0100

    align proxy settings and remove custom ssl

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,5 @@
 const {
     PROTOCOL,
-    INT_DOMAIN,
     PRIVATE_FORM_NAME,
     PRIVATE_POSTGREST_NAME,
     PRIVATE_REFDATA_URL,
@@ -13,16 +12,16 @@ const {
 const appConfig = {
     services: {
         operationalDataInternal: {
-            url: `${PROTOCOL}${PRIVATE_POSTGREST_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_POSTGREST_NAME}.${EXT_DOMAIN}`,
         },
         operationalDataExternal: {
             url: `${PROTOCOL}${PRIVATE_POSTGREST_NAME}.${EXT_DOMAIN}`,
         },
         workflow: {
-            url: `${PROTOCOL}${PRIVATE_WORKFLOW_ENGINE_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_WORKFLOW_ENGINE_NAME}.${EXT_DOMAIN}`,
         },
         form: {
-            url: `${PROTOCOL}${PRIVATE_FORM_NAME}.${INT_DOMAIN}`,
+            url: `${PROTOCOL}${PRIVATE_FORM_NAME}.${EXT_DOMAIN}`,
         },
         referenceData: {
             url: PRIVATE_REFDATA_URL,
