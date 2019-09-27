1b383456f668e000e2416d1e0da0f810226a87ba
Mark Olliver <mark@digitalpatterns.io>
2019-09-27 07:48:05 +0100

    updates pdf

--- src/form/SelectComponentVisitor.js
+++ src/form/SelectComponentVisitor.js
@@ -12,6 +12,16 @@ export default class SelectComponentVisitor {
             const value = component.data.url;
             component.data.url = this.jsonPathEvaluator.performJsonPathEvaluation({key, value},
                 dataResolveContext);
+            const bearerValue = `Bearer ${dataResolveContext.keycloakContext.accessToken}`;
+            const header = component.data.headers.find(h => h.key === 'Authorization');
+            if (header) {
+                header.value = bearerValue;
+            } else {
+                component.data.headers.push({
+                    "key": "Authorization",
+                    "value": bearerValue
+                });
+            } 
         }
     }
 }
