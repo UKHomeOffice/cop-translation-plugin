6bf2f65d003814932bd0c9c5f2b4bd47db6153e3
shepda <david@x-designs.co.uk>
2019-11-27 16:15:31 +0000

    Update IntegrityLeadService to get integrity leads by directorate and department as well as branch

--- test/services/IntegrityLeadService.test.js
+++ test/services/IntegrityLeadService.test.js
@@ -6,18 +6,9 @@ import PlatformDataService from '../../src/services/PlatformDataService';
 describe('IntegrityLeadService', () => {
   let platformDataService;
   let integrityLeads;
+  let teams;
 
   const staffTeamId = '15a13c2a-fa63-4437-b4b0-d6b070e9c17e';
-  const teams = [{
-    id: staffTeamId,
-    branchid: 11
-  }, {
-    id: '444e92aa-16e6-41fb-9642-783532f4dd84',
-    branchid: 12
-  }, {
-    id: 'bfb30c46-e7fa-4716-81b7-7687eef2c312',
-    branchid: 12
-  }];
 
   beforeEach(() => {
     platformDataService = sinon.createStubInstance(PlatformDataService);
@@ -34,6 +25,16 @@ describe('IntegrityLeadService', () => {
     }, {
       email: 'integritylead3@homeoffice.gov.uk',
       defaultteamid: '9f4bebae-ffd1-4a31-9cfd-2108d3a1423f',
+    }];
+    teams = [{
+      id: staffTeamId,
+      branchid: 11
+    }, {
+      id: '444e92aa-16e6-41fb-9642-783532f4dd84',
+      branchid: 12
+    }, {
+      id: 'bfb30c46-e7fa-4716-81b7-7687eef2c312',
+      branchid: 12
     }];
   });
 
@@ -76,12 +77,12 @@ describe('IntegrityLeadService', () => {
     });
   });
 
-  describe('branchTeamIds', () => {
+  describe('filterTeams', () => {
     it('should return the correct teams for a branch', () => {
       const integrityLeadService = new IntegrityLeadService(platformDataService);
-      const branchTeamIds = integrityLeadService.branchTeamIds(teams, 12);
+      const teamIds = integrityLeadService.filterTeams(teams, 'branchid', 12);
 
-      expect(branchTeamIds).toEqual([
+      expect(teamIds).toEqual([
         '444e92aa-16e6-41fb-9642-783532f4dd84',
         'bfb30c46-e7fa-4716-81b7-7687eef2c312'
       ]);
@@ -89,9 +90,124 @@ describe('IntegrityLeadService', () => {
 
     it('should return an empty array when teams cannot be found for a branch', () => {
       const integrityLeadService = new IntegrityLeadService(platformDataService);
-      const branchTeamIds = integrityLeadService.branchTeamIds(teams, 21);
+      const teamIds = integrityLeadService.filterTeams(teams, 'branchid', 21);
 
-      expect(branchTeamIds).toEqual([]);
+      expect(teamIds).toEqual([]);
+    });
+
+    it('should not return a match when both team branch id and the staff branch id are null', () => {
+      teams[2].branchid = null;
+
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const teamIds = integrityLeadService.filterTeams(teams, 'branchid', null);
+
+      expect(teamIds).toEqual([]);
+    });
+  });
+
+  describe('filterTeamsById', () => {
+    it('should return the correct teams filtered by branch when given a branch, directorate and department', () => {
+      teams = [{
+        id: staffTeamId,
+        branchid: 11,
+        directorateid: 12,
+        departmentid: 17
+      }, {
+        id: '2af51406-a1c2-4c2d-8475-462564ada449',
+        branchid: 11,
+        directorateid: 14,
+        departmentid: 12
+      }, {
+        id: '444e92aa-16e6-41fb-9642-783532f4dd84',
+        branchid: 12,
+        directorateid: 16,
+        departmentid: 18
+      }, {
+        id: 'bfb30c46-e7fa-4716-81b7-7687eef2c312',
+        branchid: 12,
+        directorateid: 16,
+        departmentid: 18
+      }];
+
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const teamIds = integrityLeadService.filterTeamsById(teams, { branchid: 12 });
+
+      expect(teamIds).toEqual([
+        '444e92aa-16e6-41fb-9642-783532f4dd84',
+        'bfb30c46-e7fa-4716-81b7-7687eef2c312'
+      ]);
+    });
+
+    it('should return the correct teams filtered by directorate when not given a branch', () => {
+      teams = [{
+        id: staffTeamId,
+        branchid: null,
+        directorateid: 14
+      }, {
+        id: '444e92aa-16e6-41fb-9642-783532f4dd84',
+        branchid: null,
+        directorateid: 16
+      }, {
+        id: 'bfb30c46-e7fa-4716-81b7-7687eef2c312',
+        branchid: null,
+        directorateid: 16
+      }];
+
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const teamIds = integrityLeadService.filterTeamsById(teams, { directorateid: 16 });
+
+      expect(teamIds).toEqual([
+        '444e92aa-16e6-41fb-9642-783532f4dd84',
+        'bfb30c46-e7fa-4716-81b7-7687eef2c312'
+      ]);
+    });
+
+    it('should return the correct teams filtered by department when not given a branch or directorate', () => {
+      teams = [{
+        id: staffTeamId,
+        branchid: null,
+        directorateid: null,
+        departmentid: 17
+      }, {
+        id: '444e92aa-16e6-41fb-9642-783532f4dd84',
+        branchid: null,
+        directorateid: null,
+        departmentid: 18
+      }, {
+        id: 'bfb30c46-e7fa-4716-81b7-7687eef2c312',
+        branchid: null,
+        directorateid: null,
+        departmentid: 18
+      }];
+
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const teamIds = integrityLeadService.filterTeamsById(teams, { departmentid: 18 });
+
+      expect(teamIds).toEqual([
+        '444e92aa-16e6-41fb-9642-783532f4dd84',
+        'bfb30c46-e7fa-4716-81b7-7687eef2c312'
+      ]);
+    });
+  });
+
+  describe('fallbackTeamIds', () => {
+    it('should return the team ids when given team ids', () => {
+      const teamIds = [
+        '444e92aa-16e6-41fb-9642-783532f4dd84',
+        'bfb30c46-e7fa-4716-81b7-7687eef2c312'
+      ];
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const ids = integrityLeadService.fallbackTeamIds(teamIds);
+
+      expect(ids).toEqual(teamIds);
+    });
+
+    it('should return the fallback team id when not given team ids', () => {
+      const teamIds = [];
+      const integrityLeadService = new IntegrityLeadService(platformDataService);
+      const ids = integrityLeadService.fallbackTeamIds(teamIds);
+
+      expect(ids).toEqual(integrityLeadService.fallbackTeamList);
     });
   });
 
@@ -104,9 +220,9 @@ describe('IntegrityLeadService', () => {
     });
 
     it('should return an empty string when integrity leads cannot be found', () => {
-      const branchTeamIds = ['2d97b103-67d3-46f9-8d1f-d7be8f90f2ec'];
+      const teamIds = ['2d97b103-67d3-46f9-8d1f-d7be8f90f2ec'];
       const integrityLeadService = new IntegrityLeadService(platformDataService);
-      const emails = integrityLeadService.emails(integrityLeads, branchTeamIds);
+      const emails = integrityLeadService.emails(integrityLeads, teamIds);
 
       expect(emails).toEqual('');
     });
